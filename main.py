c39b19b6b8655f48143e3db939d1c00f81d7e24a
DBenAsuli <66518359+DBenAsuli@users.noreply.github.com>
2024-08-01 20:23:04 +0300

    Initial files

---
+++ main.py
@@ -0,0 +1,7 @@
+# Advanced Topics in Online Privacy and Cybersecurity     Project
+# Dvir Ben Asuli                                          318208816
+# Siwar Mansour
+# The Hebrew University of Jerusalem                      August 2024
+
+if __name__ == '__main__':
+    pass
