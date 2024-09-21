d09d55b502156e861ae1a64ef9aec411dd4ded4c
DBenAsuli <dvir971@gmail.com>
2024-09-21 06:07:34 +0300

    Add skeleton for EXContainer

---
+++ enhanced_xcontainers.py
@@ -0,0 +1,10 @@
+# Advanced Topics in Online Privacy and Cybersecurity     Project
+# Dvir Ben Asuli                                          318208816
+# Siwar Mansour
+# The Hebrew University of Jerusalem                      September 2024
+
+from xcontainer import *
+
+class EXContainer(XContainer):
+    pass
+
